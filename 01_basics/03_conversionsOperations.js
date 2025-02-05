e7ac5f4cd9059f23de168acec8b844f1b14634a0
SupriyaUIDev <56475287+SupriyaUIDev@users.noreply.github.com>
2025-02-05 10:17:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,42 @@
+/*-------------------conversion to Number start--------------------*/
+let score = "33"
+console.log(typeof score);//both are same
+console.log(typeof (score));//both are same
+
+
+let valueInNumber = Number (score); //here we are converting variable declared above to Number
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//if we try to convert to Number ----------------------------------
+// "33" => 33 - converted to number and output is numeric value
+// "33abc" => converted to Number datatype but output is value is - NaN
+//true => 1; false=>0 
+
+/*-------------------conversion to Number End----------------------*/
+
+
+/*-------------------conversion to Boolean start--------------------*/
+let isLoggenIn = "adasf";
+console.log(isLoggenIn)
+let booleanIsLoggedIn = Boolean(isLoggenIn);
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// if we try to convert to Boolean ----------------------------------
+// 1=>true; 0=> false
+// "" => flase
+// "any_string" => true
+
+/*-------------------conversion to Boolean End----------------------*/
+
+
+
+
+/*-------------------conversion to String start--------------------*/
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
+console.log(StringNumber);
+/*-------------------conversion to String End----------------------*/
